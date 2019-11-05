041e0783ba3b190e1a3aff8f505bd7de3c55f5a3
rizalfe <r.febriantofficial@gmail.com>
2019-11-05 23:13:16 +0700

    push tugas hari ketiga

--- exercise-6.js
+++ exercise-6.js
@@ -1,7 +1,47 @@
 ////Digit Perkalian Minium////
 
 function digitPerkalianMinimum(angka) {
-    // you can only write your code here!
+    var bilangan1 = [];
+    var hasil = [];
+    for (var a = 0; a <= angka; a++) {
+        if (angka % a == 0) {
+            bilangan1.push(a);
+        }
+    }
+    var bilangan2 = []; 
+    for (var b = 0; b <= bilangan1.length; b++) {
+        if (angka % bilangan1[b] == 0) {
+            bilangan2.push(bilangan1[b]);
+        }
+    }
+    var tampung = [];
+    for (var i = 0; i < bilangan1.length/2; i++) {
+        for (var j = 0; j < bilangan2.length; j++) {
+            if (bilangan1[i]*bilangan2[j] == angka) {
+                tampung.push(`${bilangan1[i]}${bilangan2[j]}`);
+            }
+        }
+    }
+    var digit = [];
+    for (var x = 0; x < tampung.length; x++) {
+        digit.push(tampung[x].length);
+    }
+
+    //Masuk ke prose Pembandingan//
+    if (digit.length <= 1) {
+        return digit[0];
+    } else
+    if (digit.length > 1) {
+        var banding = '';
+        for (var y = 0; y < digit.length; y++) {
+            for (var z = y + 1; z < digit.length; z++) {
+                if (digit[y] > digit[z] || digit[z] == digit[y]) {
+                    banding = digit[z];
+                }
+            }
+        }
+    }
+    return banding;
 }
 
 // TEST CASES
